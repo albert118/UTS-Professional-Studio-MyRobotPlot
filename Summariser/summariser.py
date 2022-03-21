6004b396511c27ce00745f1921cdbe7e194c8a18
theQuantumMechanic <albertferguson118@gmail.com>
2022-03-21 19:10:19 +1100

    actually bloody works when you fix the typo

--- Summariser/summariser.py
+++ Summariser/summariser.py
@@ -89,7 +89,9 @@ class Summariser():
 def RunDemo():
     introduction_text="""The human coronavirus was first diagnosed in 1965 by Tyrrell and Bynoe from the respiratory tract sample of an adult with a common cold cultured on human embryonic trachea.1 Naming the virus is based on its crown-like appearance on its surface.2 Coronaviruses (CoVs) are a large family of viruses belonging to the Nidovirales order, which includes Coronaviridae, Arteriviridae, and Roniviridae families.3 Coronavirus contains an RNA genome and belongs to the Coronaviridae family.4 This virus is further subdivided into four groups, ie, the α, β, γ, and δ coronaviruses.5 α- and β-coronavirus can infect mammals, while γ- and δ- coronavirus tend to infect birds.6 Coronavirus in humans causes a range of disorders, from mild respiratory tract infections, such as the common cold to lethal infections, such as the severe acute respiratory syndrome (SARS), Middle East respiratory syndrome (MERS) and Coronavirus disease 2019 (COVID-19). The coronavirus first appeared in the form of severe acute respiratory syndrome coronavirus (SARS-CoV) in Guangdong province, China, in 20027 followed by Middle East respiratory syndrome coronavirus (MERS-CoV) isolated from the sputum of a 60-year-old man who presented symptoms of acute pneumonia and subsequent renal failure in Saudi Arabia in 2012.8 In December 2019, a β-coronavirus was discovered in Wuhan, China. The World Health Organization (WHO) has named the new disease as Coronavirus disease 2019 (COVID-19), and Coronavirus Study Group (CSG) of the International Committee has named it as SARS-CoV-2.9,10 Based on the results of sequencing and evolutionary analysis of the viral genome, bats appear to be responsible for transmitting the virus to humans"""
     sentances = Summariser(introduction_text)
-    sentances.PrintTokensFromDoc()
-    showSummaryFlag = input("Display summary (Y)?").upper().trim() == 'Y'
+    print(); print(); print()
+    showSummaryFlag = input("Display summary (Y)?").upper() == 'Y'
+    print(); print(); print()
     sentances.GetSummary(showSummaryFlag)
 
+RunDemo()
