302c7b2ab16f6b5002e13111aaa5fd8667554eae
theQuantumMechanic <albertferguson118@gmail.com>
2022-03-24 01:37:02 +1100

    limit row length for perf ceiling limit

--- ETL/etl.py
+++ ETL/etl.py
@@ -1,9 +1,16 @@
+from curses import raw
 from .extractor import Extract
 from .transformer import Transform
 
 
+MAX_JSON_SIZE = 300
+
 def DefaultPipeline(fileName: str, dropIds=[]):
     rawFrame = Extract(fileName)
-    transformedFrame = Transform(rawFrame, dropIds)
+
+    # cause im bad at regex and some of these rows are flippin' huge
+    filteredFrame = rawFrame[rawFrame.columns[0].str.len() <= MAX_JSON_SIZE & rawFrame.columns[1].str.len() <= MAX_JSON_SIZE]
+
+    transformedFrame = Transform(filteredFrame, dropIds)
 
     return transformedFrame
