61099f3133ce307256aa1b348e0e69b73ab4f40c
HughWard <hugh.ward1326@gmail.com>
2022-05-11 21:44:58 +1000

    Small changes

--- web_app.py
+++ web_app.py
@@ -3,7 +3,7 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 from .app import web_tool
-from .rudalle.image_generator import get_image
+# from .rudalle.image_generator import get_image
 
 
 app = Flask(__name__)
